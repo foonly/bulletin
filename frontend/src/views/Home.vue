e707ef61ba43976229b6a5f67bd9c694b3009ccc
Niklas Schönberg <niklas@uplink.fi>
2026-06-11 21:38:03 +0300

    fix(layout): redesign app header navigation with icons

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -137,9 +137,40 @@
 				</div>
 				<nav class="app-header__nav">
 					<span class="app-version">v{{ version }}</span>
-					<router-link to="/settings">{{ auth.user?.username }}</router-link>
-					<button class="app-header__logout" @click="handleLogout">
-						Logout
+					<span class="app-header__user">{{ auth.user?.username }}</span>
+					<router-link to="/settings" title="Settings" class="nav-link">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							class="nav-icon"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						<span>Settings</span>
+					</router-link>
+					<button
+						class="app-header__logout nav-link"
+						@click="handleLogout"
+						title="Logout"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							class="nav-icon"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						<span>Logout</span>
 					</button>
 				</nav>
 			</header>
