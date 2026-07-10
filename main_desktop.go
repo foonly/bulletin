4251e66aca10e81273b96ba0fceae766d2de2c74
Niklas Schönberg <niklas@uplink.fi>
2026-07-10 11:50:46 +0300

    feat(desktop): add application icon and linux tray support

--- main_desktop.go
+++ main_desktop.go
@@ -10,17 +10,19 @@ import (
 	"github.com/wailsapp/wails/v2/pkg/menu/keys"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/linux"
 	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
 
+//go:embed frontend/public/favicon.png
+var icon []byte
+
 func main() {
-	// Create an instance of the app structure
 	app := NewApp()
 
-	// Create a system tray menu
 	trayMenu := menu.NewMenu()
 	trayMenu.Append(menu.Text("Open Bulletin", nil, func(_ *menu.CallbackData) {
 		runtime.WindowShow(app.ctx)
@@ -30,7 +32,6 @@ func main() {
 		runtime.Quit(app.ctx)
 	}))
 
-	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "Bulletin",
 		Width:  1024,
@@ -43,14 +44,16 @@ func main() {
 		Bind: []interface{}{
 			app,
 		},
-		// Intercept close to hide to tray
 		OnBeforeClose: func(ctx context.Context) bool {
 			runtime.WindowHide(ctx)
-			return true // Prevent the application from closing
+			return true
 		},
+		// In Wails v2.13+, for Linux, assigning a Menu and setting an Icon
+		// in linux.Options often triggers the SNI (tray) integration automatically.
 		Menu: trayMenu,
-		Debug: options.Debug{
-			OpenInspectorOnStartup: true,
+		Linux: &linux.Options{
+			Icon:        icon,
+			ProgramName: "bulletin",
 		},
 	})
 
